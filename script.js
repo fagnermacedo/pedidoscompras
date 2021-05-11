8ad410c41caa20e0643b1383e192860439764e00
fagnermacedo <fagnermacedo@yahoo.com.br>
2021-05-11 13:27:42 -0300

    .

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// Função ativada através da propriedade onclick
+//Parametros passados para a função. Propriedade do botão. Ex: ('otimo', this, 'red')
+
+
 function openStatus(statusName,elmnt,color) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none"; //elementro não será mostrado
+        
     }
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
+        tablinks[i].style.backgroundColor = ""; //Elemento não terá 
     }
+
+    //Define um elemento DIV para não ser exibido. A propriedade display:none oculta o elemento.
+    //A propriedade BLOCK faz com que o elemento seja renderizado como um elemento de nível de bloco.
     document.getElementById(statusName).style.display = "block";
     elmnt.style.backgroundColor = color;
 
