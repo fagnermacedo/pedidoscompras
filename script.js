8c6d38c0e5ab5bc474a6c9b8e7f524db7ed99aa4
fagnermacedo <fagnermacedo@yahoo.com.br>
2021-05-17 15:58:04 -0300

    Adição do Formulário e função do botão Incluir
    Foi adicionado um formulário  a página HTML, para permitir a inclusão de novos pedidos. Precisa ser ajustado ainda o Checkbox e o ID dos novos pedidos incluídos.

--- script.js
+++ script.js
@@ -22,3 +22,46 @@ function openStatus(statusName,elmnt,color) {
     }
     // Obtem o elemento com o id="defaultOpen" e clica nele.
     document.getElementById("defaultOpen").click();
+
+
+    /*
+    ##############################################################################################################
+                      Informações referentes ao Formulário da página HTML e funções dos botões na página
+    ##############################################################################################################
+    */
+
+    var botaoIncluir = document.querySelector("#incluir-pedido");
+
+    botaoIncluir.addEventListener("click", function(event){
+        event.preventDefault();
+
+        var form = document.querySelector(".formulario-principal");
+
+        var nome = form.nome.value
+        var precoUnitario = form.preco.value;
+        var multiplo = form.multiplo.value;
+
+        
+        var listaTr = document.createElement("tr");
+
+        var checkBoxTd = document.createElement("td");
+        var idTd = document.createElement("td");
+        var nomeTd = document.createElement("td");
+        var precoUnitarioTd = document.createElement("td");
+        var multiploTd = document.createElement("td");
+
+        nomeTd.textContent = nome;
+        precoUnitarioTd.textContent = precoUnitario;
+        multiploTd.textContent = multiplo;
+
+        listaTr.appendChild(checkBoxTd);
+        listaTr.appendChild(idTd);
+        listaTr.appendChild(nomeTd);
+        listaTr.appendChild(precoUnitarioTd);
+        listaTr.appendChild(multiploTd);
+
+        var tabela = document.querySelector(".tabela-pedidos");
+
+        tabela.appendChild(listaTr);
+
+    });
